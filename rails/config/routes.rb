73bd6984af3c0c71596a95e50923ae84a5e0b166
taji <taji@sonicgarden.jp>
2025-10-13 15:10:11 +0900

    devise_token_auth追加

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'health_check', to: 'health_check#index'
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
